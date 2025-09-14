8c958b970c95639e5479e7c9d0cd0aac7b7f08b7
jeanmendesgit <jeanmendescontato@gmail.com>
2025-09-14 17:24:17 -0300

    NoTrabalho 14/09/25

--- Jean/Pasta 01 - ExerciciosEmAula/17 - quantas_notas.py
+++ Jean/Pasta 01 - ExerciciosEmAula/17 - quantas_notas.py
@@ -5,7 +5,7 @@ app = ctk.CTk()
 app.title('Quantidade de notas')
 
 # L resolution ..................
-width, height = 300, 530
+width, height = 300, 560
 app.geometry(f"{width}x{height}")
 app.minsize(width, height)
 app.maxsize(width, height)
@@ -29,6 +29,7 @@ h_frame = 80
 txt_label_one  = "Valor em dinheiro:"
 txt_button_one = "Verificar"
 pht_entry_one  = "Digite a quantidade em dinheiro... "
+txt_label_erro = "Atençāo! Valor inválido!"
 
 txt_frame_title_one   = "200 Reais"
 txt_frame_title_two   = "100 Reais"
@@ -46,14 +47,20 @@ buttons_color      = "#58A84F"
 buttons_hover      = "#387E30"
 buttons2_color     = "#34656C"
 buttons2_hover     = "#588D93"
-text_disable_color = "gray"
+text_wrong_color   = "#A84A40"
 text_right_color   = "#90D288"
 tview_fg_color     = "#354543"
+text_disable_color = "gray"
 
 # Functions
+def fix(value):
+    if ',' in value:
+        value = value.replace(',', '.')
+    return value
+
 def data_processing():
     try:
-        value = int(entry_one.get())
+        value = float(fix(entry_one.get()))
         
         notes_200 = value // 200
         value    -= notes_200 * 200
@@ -72,16 +79,30 @@ def data_processing():
         notes_1   = value // 1
         value    -= notes_1 * 1
 
-        frame_value_one.configure(text=str(notes_200), text_color=text_right_color)  if notes_200 != 0 else frame_value_one.configure(text="0", text_color=text_disable_color)
-        frame_value_two.configure(text=str(notes_100), text_color=text_right_color)  if notes_100 != 0 else frame_value_two.configure(text="0", text_color=text_disable_color)
-        frame_value_three.configure(text=str(notes_50), text_color=text_right_color) if notes_50 != 0 else frame_value_three.configure(text="0", text_color=text_disable_color)
-        frame_value_four.configure(text=str(notes_20), text_color=text_right_color)  if notes_20 != 0 else frame_value_four.configure(text="0", text_color=text_disable_color)
-        frame_value_five.configure(text=str(notes_10), text_color=text_right_color)  if notes_10 != 0 else frame_value_five.configure(text="0", text_color=text_disable_color)
-        frame_value_six.configure(text=str(notes_5), text_color=text_right_color)    if notes_5 != 0 else frame_value_six.configure(text="0", text_color=text_disable_color)
-        frame_value_seven.configure(text=str(notes_2), text_color=text_right_color)  if notes_2 != 0 else frame_value_seven.configure(text="0", text_color=text_disable_color)
-        frame_value_eight.configure(text=str(notes_1), text_color=text_right_color)  if notes_1 != 0 else frame_value_eight.configure(text="0", text_color=text_disable_color)
+        frame_value_one.configure(text=str(int(notes_200)), text_color=text_right_color)  if notes_200 != 0 else frame_value_one.configure(text="0", text_color=text_disable_color)
+        frame_value_two.configure(text=str(int(notes_100)), text_color=text_right_color)  if notes_100 != 0 else frame_value_two.configure(text="0", text_color=text_disable_color)
+        frame_value_three.configure(text=str(int(notes_50)), text_color=text_right_color) if notes_50 != 0 else frame_value_three.configure(text="0", text_color=text_disable_color)
+        frame_value_four.configure(text=str(int(notes_20)), text_color=text_right_color)  if notes_20 != 0 else frame_value_four.configure(text="0", text_color=text_disable_color)
+        frame_value_five.configure(text=str(int(notes_10)), text_color=text_right_color)  if notes_10 != 0 else frame_value_five.configure(text="0", text_color=text_disable_color)
+        frame_value_six.configure(text=str(int(notes_5)), text_color=text_right_color)    if notes_5 != 0 else frame_value_six.configure(text="0", text_color=text_disable_color)
+        frame_value_seven.configure(text=str(int(notes_2)), text_color=text_right_color)  if notes_2 != 0 else frame_value_seven.configure(text="0", text_color=text_disable_color)
+        frame_value_eight.configure(text=str(int(notes_1)), text_color=text_right_color)  if notes_1 != 0 else frame_value_eight.configure(text="0", text_color=text_disable_color)
+
+        label_erro.grid_forget()
     except ValueError:
-        pass
+        label_erro.grid(column=0, columnspan=3,
+               row=3,
+               padx=margin,
+               sticky="new")
+        
+        frame_value_one.configure(text="0", text_color=text_disable_color)
+        frame_value_two.configure(text="0", text_color=text_disable_color)
+        frame_value_three.configure(text="0", text_color=text_disable_color)
+        frame_value_four.configure(text="0", text_color=text_disable_color)
+        frame_value_five.configure(text="0", text_color=text_disable_color)
+        frame_value_six.configure(text="0", text_color=text_disable_color)
+        frame_value_seven.configure(text="0", text_color=text_disable_color)
+        frame_value_eight.configure(text="0", text_color=text_disable_color)
 
 # Fields
 
@@ -96,6 +117,10 @@ button_one = ctk.CTkButton(app, command=data_processing,
                            fg_color=buttons_color,
                            hover_color=buttons_hover,
                            corner_radius=20)
+
+label_erro = ctk.CTkLabel(app,
+                          text=txt_label_erro,
+                          text_color=text_wrong_color)
 
 # L frame ......................................
 
@@ -255,42 +280,42 @@ button_one.grid(column=0, columnspan=3,
 # L frame..............................................
 
 frame_one.grid(column=0,
-               row=3,
+               row=4,
                padx=(margin, margin // 2), pady=spacing,
                sticky="new")
 
 frame_two.grid(column=2,
-               row=3,
+               row=4,
                padx=(margin // 2, margin), pady=spacing,
                sticky="new")
 
 frame_three.grid(column=0,
-               row=4,
+               row=5,
                padx=(margin, margin // 2), pady=spacing,
                sticky="new")
 
 frame_four.grid(column=2,
-               row=4,
+               row=5,
                padx=(margin // 2, margin), pady=spacing,
                sticky="new")
 
 frame_five.grid(column=0,
-               row=5,
+               row=6,
                padx=(margin, margin // 2), pady=spacing,
                sticky="new")
 
 frame_six.grid(column=2,
-               row=5,
+               row=6,
                padx=(margin // 2, margin), pady=spacing,
                sticky="new")
 
 frame_seven.grid(column=0,
-               row=6,
+               row=7,
                padx=(margin, margin // 2), pady=spacing,
                sticky="new")
 
 frame_eight.grid(column=2,
-               row=6,
+               row=7,
                padx=(margin // 2, margin), pady=spacing,
                sticky="new")
 
@@ -298,126 +323,126 @@ frame_eight.grid(column=2,
 
 # l icon ..........................................................
 frame_icon_one.grid(column=0,
-                    row=3,
+                    row=4,
                     padx=margin, pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_two.grid(column=2,
-                    row=3,
+                    row=4,
                     padx=(0, margin * 2), pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_three.grid(column=0,
-                    row=4,
+                    row=5,
                     padx=margin, pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_four.grid(column=2,
-                    row=4,
+                    row=5,
                     padx=(0, margin * 2), pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_five.grid(column=0,
-                    row=5,
+                    row=6,
                     padx=margin, pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_six.grid(column=2,
-                    row=5,
+                    row=6,
                     padx=(0, margin * 2), pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_seven.grid(column=0,
-                    row=6,
+                    row=7,
                     padx=margin, pady=(0, spacing * 2),
                     sticky="es")
 
 frame_icon_eight.grid(column=2,
-                    row=6,
+                    row=7,
                     padx=(0, margin * 2), pady=(0, spacing * 2),
                     sticky="es")
 
 # L title ....................................................
 
 frame_title_one.grid(column=0,
-                     row=3,
+                     row=4,
                      padx=(margin*1.5, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_two.grid(column=2,
-                     row=3,
+                     row=4,
                      padx=(margin, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_three.grid(column=0,
-                     row=4,
+                     row=5,
                      padx=(margin*1.5, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_four.grid(column=2,
-                     row=4,
+                     row=5,
                      padx=(margin, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_five.grid(column=0,
-                     row=5,
+                     row=6,
                      padx=(margin*1.5, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_six.grid(column=2,
-                     row=5,
+                     row=6,
                      padx=(margin, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_seven.grid(column=0,
-                     row=6,
+                     row=7,
                      padx=(margin*1.5, 0), pady=(spacing, 0),
                      sticky="nw")
 
 frame_title_eight.grid(column=2,
-                     row=6,
+                     row=7,
                      padx=(margin, 0), pady=(spacing, 0),
                      sticky="nw")
 
 # L value ...........................................................
 
 frame_value_one.grid(column=0,
-                     row=3,
+                     row=4,
                      padx=(margin*1.5, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_two.grid(column=2,
-                     row=3,
+                     row=4,
                      padx=(margin, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_three.grid(column=0,
-                     row=4,
+                     row=5,
                      padx=(margin*1.5, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_four.grid(column=2,
-                     row=4,
+                     row=5,
                      padx=(margin, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_five.grid(column=0,
-                     row=5,
+                     row=6,
                      padx=(margin*1.5, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_six.grid(column=2,
-                     row=5,
+                     row=6,
                      padx=(margin, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_seven.grid(column=0,
-                     row=6,
+                     row=7,
                      padx=(margin*1.5, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
 frame_value_eight.grid(column=2,
-                     row=6,
+                     row=7,
                      padx=(margin, 0), pady=(0, spacing * 1.25),
                      sticky="sw")
 
