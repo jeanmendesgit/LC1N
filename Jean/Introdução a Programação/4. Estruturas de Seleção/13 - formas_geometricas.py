9f29bc31222c1c3cb63e0b78124d986b846e10c9
jeanmendesgit <jeanmendescontato@gmail.com>
2025-09-23 17:51:39 -0300

    EmCasa 23/09/25

---
+++ Jean/Introdução a Programação/4. Estruturas de Seleção/13 - formas_geometricas.py
@@ -0,0 +1,79 @@
+# Programa com ferramentas simples de apoio as aulas de matematica
+
+import functions as fn
+
+# Variaveis
+PI = 3.14159265359
+
+title   = "Calcular áreas de Figuras Geométricas"
+spacing = 60
+
+formulas = {1 : "A = L²",
+            2 : "A = b • h",
+            3 : "A = b • h / 2",
+            4 : "A = π • r²"}
+
+def main():
+    while True:   
+        fn.header(title, spacing)
+        
+        print("Qual figura será calculada:")
+        print("1) Quadrado\n2) Retângulo\n3) Triângulo\n4) Círculo\n5) Sair\n")
+        
+        try:
+            user_choice = int(input("Digite o número da opção: "))
+            
+            match user_choice:
+                # Quadrado
+                case 1:
+                    side = float(input("\nDigite a medida do lado do quadrado: "))
+                    
+                    area = side * side
+                    
+                    output = f"A = {side}² = {area}"
+                # Retângulo
+                case 2:
+                    base   = float(input("\nDigite a medida da Base: "))
+                    height = float(input("\nDigite a medida da Altura: "))
+                        
+                    area = base * height
+                    
+                    output = f"A = {base} • {height} = {area}"
+                # Triângulo
+                case 3:
+                    base   = float(input("\nDigite a medida da Base: "))
+                    height = float(input("\nDigite a medida da Altura: "))
+                        
+                    area = base * height / 2
+                    
+                    output = f"A = {base} • {height} / 2 = {area:.2f}"
+                # Círculo
+                case 4:
+                    radius = float(input("\nDigite a medida da Raio: "))
+                        
+                    area = PI * (radius * radius)
+                    
+                    output = f"A = π • {radius}² = {area:.2f}"
+                case 5:
+                    fn.highlight("Até mais!", spacing)
+                    break
+                case _:
+                    print("\nOpção não encontrada!\n")
+                    
+            fn.highlight(f"Fórmula: {formulas[user_choice]}", spacing)
+            fn.highlight(f"Calculo: {output}", spacing)
+            fn.footer(spacing)
+        except ValueError:
+            print("\nErro: Valor inválido!\n")
+            
+            input("Pressione ENTER para retonar ao menu\n")
+            
+            main()
+            
+        input("\nPressione ENTER para retonar ao menu\n")
+        
+main()
+
+
+    
+            
